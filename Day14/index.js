21f52133593a9f3b55908d134b21bdfab3919998
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-08-24 11:42:51 +0530

    updated till imports

---
+++ Day14/index.js
@@ -0,0 +1,17 @@
+export const firstName = 'ritesh';
+export const address = 'jaipur';
+
+//we have two type of the export 
+//NOTE named exports
+//NOTE default exports
+
+export default function hello(){
+    console.log('hello')
+}
+
+
+//named exports
+// {
+//     firstName : 'ritesh',
+//     address : 'jaipur'
+// }
--- Day14/script.js
+++ Day14/script.js
@@ -1,12 +1,24 @@
-console.log(window) ;
+//NOTE named import
+import {firstName , address} from './index.js' ;
+//NOTE default import
+import hello from './index.js'
 
-setTimeout(()=>{
-console.log('hello')
-},3000)
 
-const clrInterval = setInterval(()=>{
-    console.log('run in every 1 sec')
-},1000)
+
+console.log(firstName, address) ;
+hello() ;
+
+
+
+console.log(window);
+
+setTimeout(() => {
+  console.log('hello');
+}, 3000);
+
+const clrInterval = setInterval(() => {
+  console.log('run in every 1 sec');
+}, 1000);
 
 //to clear the execution of the setInterval
 clearInterval(clrInterval);
@@ -39,7 +51,7 @@ clearInterval(clrInterval);
 //    setTimeout(()=>{
 //   console.log('step1 complete');
 //   callback();
-//    },1000) 
+//    },1000)
 
 // }
 
@@ -47,7 +59,7 @@ clearInterval(clrInterval);
 //    setTimeout(()=>{
 //   console.log('step2 complete');
 //   callback();
-//    },1000) 
+//    },1000)
 
 // }
 
@@ -55,17 +67,16 @@ clearInterval(clrInterval);
 //    setTimeout(()=>{
 //   console.log('step3 complete');
 //   callback();
-//    },1000) 
+//    },1000)
 
 // }
 // function step4(){
 //    setTimeout(()=>{
 //   console.log('step4 complete');
-  
-//    },1000) 
+
+//    },1000)
 
 // }
-
 
 // step1(()=>{
 //     step2(()=>{
@@ -75,42 +86,80 @@ clearInterval(clrInterval);
 //     })
 // })
 
-// //Promises : 
+// //Promises :
 
 // // promises = placeholder ,it is a js object
 // //promises = always return from a function
-// //promises have different stages => fulfilled , rejected 
+// //promises have different stages => fulfilled , rejected
 
-// //creating a promise 
+// //creating a promise
 
-const willYouPassInExam = new Promise((resolve,reject)=>{
-    reject('no')
-})
-console.log(willYouPassInExam)
-
+const willYouPassInExam = new Promise((resolve, reject) => {
+  reject('no');
+});
+console.log(willYouPassInExam);
 
 // console.log(fetch('https://restcountries.com/v3.1/all'))
 
-
-function data(){
-
-    return new Promise((resolve,reject)=>{
-      const data ='failed to fetch data' 
-      reject(data);
-    })
+function data() {
+  return new Promise((resolve, reject) => {
+    const data = 'failed to fetch data';
+    reject(data);
+  });
 }
 
-console.log(data())
-data().then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.error(err)
-}).finally(()=>{
-    console.log('run everytime')
-})
+console.log(data());
+data()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+  .finally(() => {
+    console.log('run everytime');
+  });
 
 //REVIEW  in the case of the promise fulfilled we have to handle with .then() method
 //REVIEW in the case of the promise is rejected we have to handle with .catch() method
 //REVIEW finally() will always run in both cases.
 
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+//async await ;
+//new version of promise by using async await your code look more like synchronous line by line
+
+//NOTE async await both are keywords
+//NOTE to use the await keyword you always have to make the function asynchronous by using async keyword before it ;
+
+//NOTE REGULAR PROMISES
+//  fetch('https://restcountries.com/v3.1/all').then((res)=>{
+// return res.json()
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+//NOTE ASYNC AWAIT ;
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('https://restcountries.com/v3.1/all');
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+}; 
+
+fetchData();
